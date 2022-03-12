537a5a6d4690076884801eaf594fde0cafdb6be3
Joe Wezorek <jwezorek@gmail.com>
2022-03-12 14:40:52 -0800

    contour extraction

--- src/imgproc.cpp
+++ src/imgproc.cpp
@@ -42,6 +42,36 @@ namespace {
             r::to<std::map<uchar, cv::Mat>>();
     }
 
+    struct contours_trees {
+        std::vector<std::vector<cv::Point>> contours;
+        std::vector<cv::Vec4i> hierarchy;
+    };
+
+    std::map<uchar, contours_trees> gray_level_contours(const cv::Mat& input) {
+        auto planes = gray_planes(input);
+        return planes |
+            rv::transform(
+                [](const auto& pair)->std::map<uchar, contours_trees>::value_type {
+                    const auto& [gray, mat] = pair;
+                    contours_trees output;
+                    cv::findContours(mat, output.contours, output.hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+                    return { gray, std::move(output) };
+                }
+            ) |
+            r::to<std::map<uchar, contours_trees>>();
+    }
+
+    cv::Mat paint_contours(int wd, int hgt, const std::map<uchar, contours_trees>& cont) {
+        auto img = cv::Mat(hgt, wd, CV_8UC1, 255);
+        for (const auto& [gray, c] : cont) {
+            int n = static_cast<int>(c.contours.size());
+            for ( int i = 0; i < n; ++i) {
+                cv::drawContours(img, c.contours, i, gray, cv::FILLED, 8, c.hierarchy);
+            }
+        }
+        return img;
+    }
+
 }
 
 cv::Mat ch::do_segmentation(const cv::Mat& input, int sigmaS, float sigmaR, int minSize) {
@@ -60,12 +90,8 @@ void ch::debug() {
     cv::Mat img = cv::imread("C:\\test\\dunjon.png");
 
     cv::Mat mat = ch::do_segmentation(img, 8.0f, 3.0f, 12);
-    auto levels = gray_planes(mat);
+    auto contours = gray_level_contours(mat);
+    auto output = paint_contours(mat.cols, mat.rows, contours);
 
-    for (const auto [lev, img] : levels) {
-        cv::imshow("level", img);
-        cv::waitKey(0);
-    }
-
-    std::cout << levels.size() << "\n";
+    cv::imshow("contours", output);
 }
