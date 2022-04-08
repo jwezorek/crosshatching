717c92bc743a3d62b8525d636c2ac166a562c422
Joe Wezorek <jwezorek@gmail.com>
2022-04-08 15:12:25 -0700

    broke out sliders into a separate TU.

--- src/ui/settingctrls.cpp
+++ src/ui/settingctrls.cpp
@@ -1,125 +1,6 @@
 #include "settingctrls.hpp"
 #include "../crosshatching/util.hpp"
 
-double ui::float_value_slider::range() const {
-	return static_cast<double>(
-		slider_->maximum()
-	);
-}
-
-int ui::float_value_slider::value_to_position(double val) const {
-	return val_to_pos_(val, range(), min_, max_);
-}
-
-double ui::float_value_slider::position_to_value(int pos) const {
-	return pos_to_val_(pos, range(), min_, max_);
-}
-
-double ui::float_value_slider::value() const {
-	return position_to_value(slider_->sliderPosition());
-}
-
-ui::float_value_slider::float_value_slider(const QString& txt, double min, double max, double init_val, int range, 
-		val_to_pos_fn val_to_pos, pos_to_val_fn pos_to_val) :
-	min_(min), max_(max),
-	val_to_pos_(val_to_pos), pos_to_val_(pos_to_val)
-{
-	QVBoxLayout* layout = new QVBoxLayout();
-	QLabel* lbl_title = new QLabel(txt);
-
-	slider_ = new QSlider();
-	slider_->setOrientation(Qt::Vertical);
-	slider_->setMinimum(0);
-	slider_->setMaximum(range);
-	slider_->setTickPosition(QSlider::TicksBothSides);
-	slider_->setTickInterval(10);
-
-	slider_->setSliderPosition(value_to_position(init_val));
-
-	lbl_val_ = new QLabel(ch::to_string(init_val, 2).c_str());
-
-	layout->addWidget(lbl_title);
-	layout->addWidget(slider_);
-	layout->addWidget(lbl_val_);
-	this->setLayout(layout);
-
-	connect(slider_, &QSlider::valueChanged, this, &float_value_slider::handle_position_change);
-	connect(slider_, &QSlider::sliderReleased, this, &float_value_slider::handle_released);
-}
-
-void ui::float_value_slider::set(double value) {
-	slider_->setSliderPosition(value_to_position(value));
-}
-
-void ui::float_value_slider::handle_position_change(int new_pos) {
-	double new_value = position_to_value(new_pos);
-	lbl_val_->setText(ch::to_string(new_value, 2).c_str());
-	value_changed(new_value);
-}
-
-void ui::float_value_slider::handle_released() {
-	slider_released();
-}
-
-int ui::float_value_slider::linear_value_to_position(double val, double range, double min, double max) {
-	return static_cast<int>(range * ((val - min) / (max - min)));
-}
-
-double ui::float_value_slider::linear_position_to_value(int pos, double range, double min, double max) {
-	return min + ((max - min) * pos) / range;
-}
-
-int exp_val_to_pos(double val, double range, double min, double max) {
-	double base = std::pow(max, 1.0 / range);
-	return std::log(val) / std::log(base);
-}
-
-double exp_pos_to_val(int pos, double range, double min, double max) {
-	double base = std::pow(max, 1.0 / range);
-	return std::pow(base, static_cast<double>(pos));
-}
-
-ui::int_value_slider::int_value_slider(const QString& txt, int min, int max, int init_val) 
-{
-	QVBoxLayout* layout = new QVBoxLayout();
-	QLabel* lbl_title = new QLabel(txt);
-
-	slider_ = new QSlider();
-	slider_->setOrientation(Qt::Vertical);
-	slider_->setMinimum(min);
-	slider_->setMaximum(max);
-	slider_->setTickPosition(QSlider::TicksBothSides);
-	slider_->setTickInterval(1);
-
-	slider_->setSliderPosition(init_val);
-
-	lbl_val_ = new QLabel(std::to_string(init_val).c_str());
-
-	layout->addWidget(lbl_title);
-	layout->addWidget(slider_);
-	layout->addWidget(lbl_val_);
-	this->setLayout(layout);
-
-	connect(slider_, &QSlider::valueChanged, this, &int_value_slider::handle_position_change);
-	connect(slider_, &QSlider::sliderReleased, this, &int_value_slider::handle_released);
-}
-
-void ui::int_value_slider::set(int val) {
-	slider_->setSliderPosition(val);
-}
-
-int ui::int_value_slider::value() const {
-	return slider_->sliderPosition();
-}
-
-void ui::int_value_slider::handle_position_change(int pos) {
-	lbl_val_->setText(std::to_string(pos).c_str());
-	value_changed(pos);
-}
-
-void ui::int_value_slider::handle_released() {
-	slider_released();
-}
 /*---------------------------------------------------------------------------------------------------------------------------*/
 
 ui::image_processing_pipeline_item::image_processing_pipeline_item(QString title, int index) :
@@ -171,7 +52,7 @@ void ui::scale_and_contrast::populate() {
 
 	QGroupBox* contrast_box = new QGroupBox("contrast");
 	QHBoxLayout* cb_layout = new QHBoxLayout();
-	cb_layout->addWidget(contrast_slider_ = new ui::float_value_slider("beta", 1, 20, 1, 1000, exp_val_to_pos, exp_pos_to_val));
+	cb_layout->addWidget(contrast_slider_ = new ui::float_value_slider("beta", 1, 20, 1, 1000, ui::exp_val_to_pos, ui::exp_pos_to_val));
 	cb_layout->addWidget(thresh_slider_ = new ui::float_value_slider("sigma", 0, 1, 0.5));
 	contrast_box->setLayout(cb_layout);
 
--- src/ui/settingctrls.hpp
+++ src/ui/settingctrls.hpp
@@ -1,66 +1,12 @@
 #pragma once
 
-#include <opencv2/core.hpp>
 #include <QtWidgets>
+#include "float_value_slider.h"
+#include <opencv2/core.hpp>
 #include <tuple>
 #include <functional>
+
 namespace ui {
-
-    using val_to_pos_fn = std::function<int(double val, double range, double min, double max)>;
-    using pos_to_val_fn = std::function<double(int val, double range, double min, double max)>;
-
-    class float_value_slider : public QWidget {
-
-        Q_OBJECT
-
-    public:
-        static int linear_value_to_position(double val, double range, double min, double max);
-        static double linear_position_to_value(int pos, double range, double min, double max);
-
-        float_value_slider(const QString& txt, double min, double max, double init_val, int range = 1000,
-            val_to_pos_fn val_to_pos = linear_value_to_position, pos_to_val_fn pos_to_val = linear_position_to_value);
-        void set(double value);
-        double value() const;
-
-    signals:
-        void value_changed(double new_val);
-        void slider_released();
-
-    private:
-        val_to_pos_fn val_to_pos_;
-        pos_to_val_fn pos_to_val_;
-        double min_;
-        double max_;
-        QSlider* slider_;
-        QLabel* lbl_val_;
-
-        double range() const;
-        int value_to_position(double val) const;
-        double position_to_value(int pos) const;
-
-        void handle_position_change(int pos);
-        void handle_released();
-
-    };
-
-    class int_value_slider : public QWidget {
-
-        Q_OBJECT
-
-    private:
-        QSlider* slider_;
-        QLabel* lbl_val_;
-
-        void handle_position_change(int pos);
-        void handle_released();
-    public:
-        int_value_slider(const QString& txt, int min, int max, int init_val);
-        void set(int val);
-        int value() const;
-    signals:
-        void value_changed(int new_val);
-        void slider_released();
-    };
 
     class image_processing_pipeline_item : public QWidget {
         Q_OBJECT
